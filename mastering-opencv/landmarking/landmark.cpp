c72a0c8d313d9800840ba96fac8566f86cd745ca
JesseHarder <jhharder@scu.edu>
2015-02-04 16:36:03 -0800

    Added undo feature to landmarking.

--- mastering-opencv/landmarking/landmark.cpp
+++ mastering-opencv/landmarking/landmark.cpp
@@ -7,6 +7,8 @@
 
 #define NUM_POINTS 76
 
+void copyPointsArray(int *src, int* dest);
+void undo();
 void draw();
 void onMouse( int event, int x, int y, int, void* );
 using namespace cv;
@@ -14,6 +16,7 @@ using namespace std;
 
 static Mat img;
 static int points[] = {201,348,201,381,202,408,209,435,224,461,241,483,264,498,292,501,319,493,338,470,353,448,363,423,367,395,366,371,357,344,355,316,340,311,325,318,309,328,327,324,342,317,217,328,231,323,250,327,269,333,251,334,233,331,229,345,240,337,262,349,242,352,241,344,346,337,330,330,318,341,334,344,330,336,280,344,278,381,264,399,273,406,293,409,316,399,321,392,304,376,296,342,279,402,310,399,251,431,268,427,284,425,293,425,302,423,316,425,329,426,320,442,309,451,295,454,278,452,263,442,277,440,293,442,313,437,313,429,293,432,277,431,293,436,295,395,234,341,251,343,252,350,235,348,338,333,324,335,326,342,340,340};
+static int prevPoints[NUM_POINTS*2];
 
 int main(int argc, char **argv)
 {
@@ -23,6 +26,7 @@ int main(int argc, char **argv)
     }
     
     img = imread(argv[1]);
+    copyPointsArray(points, prevPoints);
     
     namedWindow("Landmarks");
     setMouseCallback("Landmarks", onMouse, 0 );
@@ -32,6 +36,7 @@ int main(int argc, char **argv)
         
         int c = waitKey(10);
         if (c == 'q') break;
+        else if (c == 'z') undo();
     }
     
     string fileName = argv[1];
@@ -46,6 +51,18 @@ int main(int argc, char **argv)
     }
     
     myfile << points[NUM_POINTS*2-1] << endl;
+}
+
+void copyPointsArray(int *src, int* dest)
+{
+    for (int i = 0; i < 2*NUM_POINTS; i++) {
+        dest[i] = src[i];
+    }
+}
+
+void undo()
+{
+    copyPointsArray(prevPoints, points);
 }
 
 int getNearestPoint(int x, int y)
@@ -81,7 +98,7 @@ void draw()
         circle(newImg, Point(x,y), 3, Scalar(0, 0, 255), -1, 8);
     }
     
-    imshow("Landmarks", newImg); 
+    imshow("Landmarks", newImg);
 }
 
 void onMouse( int event, int x, int y, int, void* )
@@ -91,6 +108,7 @@ void onMouse( int event, int x, int y, int, void* )
     
     if (event == CV_EVENT_LBUTTONDOWN)
     {
+        copyPointsArray(points, prevPoints);
         nearestPointIndex = getNearestPoint(x,y);
         mouseDown = true;
     }
@@ -101,6 +119,7 @@ void onMouse( int event, int x, int y, int, void* )
     }
     else if (mouseDown && event == CV_EVENT_MOUSEMOVE)
     {
+
         points[2*nearestPointIndex] = x;
         points[2*nearestPointIndex + 1] = y;
     }
