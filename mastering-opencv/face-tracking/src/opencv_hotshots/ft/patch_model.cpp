8682ce2aa3f0b33da07ceb42d165a74693043a58
Alex DeBoni <adeboni@scu.edu>
2015-03-11 15:28:15 -0700

    gpu testing

--- mastering-opencv/face-tracking/src/opencv_hotshots/ft/patch_model.cpp
+++ mastering-opencv/face-tracking/src/opencv_hotshots/ft/patch_model.cpp
@@ -200,8 +200,7 @@ vector<Point2f> patch_models::calc_peaks(const Mat &im, const vector<Point2f> &p
 	vector<Point2f> pts = this->apply_simil(this->inv_simil(S), points);
 //	vector<Point2f> pts = this->apply_simil(Mat(this->inv_simil(GpuMat(S))), points);
 
-	Mat A(2, 3, CV_32F);
-	GpuMat I, gpIm(im);
+	Mat I, A(2, 3, CV_32F);
 	Point maxLoc;
     for (int i = 0; i < n; i++) {
         Size wsize = ssize + patches[i].patch_size();
@@ -213,8 +212,8 @@ vector<Point2f> patch_models::calc_peaks(const Mat &im, const vector<Point2f> &p
         A.fl(0, 2) = pt.fl(2 * i, 0) - (A.fl(0,0) * (wsize.width-1)/2 + A.fl(0,1)*(wsize.height-1)/2);
         A.fl(1, 2) = pt.fl(2 * i + 1, 0) - (A.fl(1,0) * (wsize.width-1)/2 + A.fl(1,1)*(wsize.height-1)/2);
 		
-		gpu::warpAffine(gpIm, I, A, wsize, INTER_LINEAR+WARP_INVERSE_MAP);
-		gpu::minMaxLoc(patches[i].calc_response(I), 0, 0, 0, &maxLoc);
+		warpAffine(im, I, A, wsize, INTER_LINEAR+WARP_INVERSE_MAP);
+		minMaxLoc(patches[i].calc_response(I), 0, 0, 0, &maxLoc);
         pts[i] = Point2f(pts[i].x + maxLoc.x - 0.5*ssize.width, pts[i].y + maxLoc.y - 0.5*ssize.height);
     }
     return this->apply_simil(S,pts);
